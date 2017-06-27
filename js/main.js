7a910af3c1a7835142cecd4a3d61393b089677c7
MG <mg71645@gmail.com>
2017-06-27 22:47:41 +0600

    Added link animation; Moved scripts to main.js

--- js/main.js
+++ js/main.js
@@ -123,3 +123,90 @@ function setPrices() {
   $('.burabay-3').html(prices[4][0]);
   $('.burabay-4').html(prices[4][1]);
 }
+
+function scrollTo(elem) {
+  if($(elem)) {
+    var destination = $(elem).offset().top;
+    $("html,body").animate({scrollTop: destination}, 800);
+  }
+}
+
+function book(tour) {
+  openModal('book');
+  if (tour) { $('#bookingModal select').val(tour) };
+  getPrices();
+}
+
+function openModal(id) {
+  document.getElementById('modal-body').innerHTML = document.getElementById(id).innerHTML;
+  setPrices();
+  $('html').css('overflow', 'hidden');
+  $('#modal').css('overflow', 'auto');
+  $('#modal').removeClass('hidden');
+  setTimeout( function() {
+    $('#modal').addClass('show');
+  }, 1 );
+}
+
+function closeModal() {
+  $('html').css('overflow', 'auto');
+  $('#modal').css('overflow', 'hidden');
+  $('#modal').removeClass('show');
+  setTimeout( function() {
+    $('#modal').addClass('hidden');
+  }, 500 );
+}
+
+function sendForm(url) {
+  if ( $('#booking-form input[name="email"]').val() || $('#booking-form input[name="whatsapp"]').val() || $('#booking-form input[name="telegram"]').val() ) {
+    $('#booking-form-button').prop('disabled', true);
+    $('#booking-form-button').html('Booking...');
+    jQuery.ajax({
+      url: url,
+      type: "POST",
+      dataType: "html",
+      data: jQuery("#booking-form").serialize(),
+      success: function(response) {
+        setTimeout( function() {
+          openModal('success');
+        }, 1000 );
+      },
+      error: function(response) {
+        $('#popup_main_cta').html('Try again!');
+        setTimeout( function() {
+          $('#booking-form-button').prop('disabled', false);
+          $('#booking-form-button').prop('value', 'Book now');
+        }, 1000 );
+      }
+    });
+  } else {
+    alert('Please specify your email, whatsapp or telegram');
+  }
+}
+
+function sendForm2(url) {
+  if ( $('#bookingModal input[name="email"]').val() || $('#bookingModal input[name="whatsapp"]').val() || $('#bookingModal input[name="telegram"]').val() ) {
+    $('#bookingModal-button').prop('disabled', true);
+    $('#bookingModal-button').html('Booking...');
+    jQuery.ajax({
+      url: url,
+      type: "POST",
+      dataType: "html",
+      data: jQuery("#bookingModal-form").serialize(),
+      success: function(response) {
+        setTimeout( function() {
+          openModal('success');
+        }, 1000 );
+      },
+      error: function(response) {
+        $('#bookingModal-button').html('Try again!');
+        setTimeout( function() {
+          $('#bookingModal-button').prop('disabled', false);
+          $('#bookingModal-button').prop('value', 'Book now');
+        }, 1000 );
+      }
+    });
+  } else {
+    alert('Please specify your email, whatsapp or telegram');
+  }
+}
